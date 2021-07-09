a101f993ffc799a0237df65108f083a06f5c912c
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2021-07-09 06:59:49 -0600

    Fix async issues when fetching data via gcdm
    From @flynnplatt - thank you!

--- grpc-web/src/main/js/index.js
+++ grpc-web/src/main/js/index.js
@@ -17,54 +17,59 @@ const {GridDataRequest} = require('./gcdm_grid_pb');
 var gcdmService = new GcdmClient('http://localhost:16110');
 var datasetLocation = '/data/grid/20210317090000-JPL-L4_GHRSST-SSTfnd-MUR25-GLOB-v02.0-fv04.2.nc'
 
-async function subset(gridName, lowerLeftLon, lowerLeftLat, lonWidth, latWidth) {
-  var gridDataRequest = new GridDataRequest();
-  gridDataRequest.setLocation(datasetLocation);
-  // get empty map to hold subset parameters
-  let subset = gridDataRequest.getSubsetMap(false);
-  // request grid "gridName" for subset
-  subset.set("gridName", gridName)
-  // bounding box is defined as string "lat,lon,latWidth,lonWidth"
-  let subsetString = lowerLeftLat + "," + lowerLeftLon + "," + latWidth + "," + lonWidth;
-  // bug in netCDF-Java 6 - need to fix before this will work
-  // set spatial subset
-  //subset.set("latlonBB", subsetString)
-  // set horizontal stride (stride in both lat and lon)
-  subset.set("horizStride", "10");
-  // set the subset map on the request
-  gridDataRequest.subsetMap = subset;
+function subset(gridName, lowerLeftLon, lowerLeftLat, lonWidth, latWidth) {
+  return new Promise((resolve, reject) => {
+    try {
+      var gridDataRequest = new GridDataRequest();
+      gridDataRequest.setLocation(datasetLocation);
+      // get empty map to hold subset parameters
+      let subset = gridDataRequest.getSubsetMap(false);
+      // request grid "gridName" for subset
+      subset.set("gridName", gridName)
+      // bounding box is defined as string "lat,lon,latWidth,lonWidth"
+      let subsetString = lowerLeftLat + "," + lowerLeftLon + "," + latWidth + "," + lonWidth;
+      // bug in netCDF-Java 6 - need to fix before this will work
+      // set spatial subset
+      //subset.set("latlonBB", subsetString)
+      // set horizontal stride (stride in both lat and lon)
+      //subset.set("horizStride", "10");
+      // set the subset map on the request
+      gridDataRequest.subsetMap = subset;
 
-  // make the request and obtain the return stream
-  let dataStream = gcdmService.getGridData(gridDataRequest, {});
+      // make the request and obtain the return stream
+      let dataStream = gcdmService.getGridData(gridDataRequest, {});
 
-  // setup the handlers for the stream
-  let data = dataStream.on('data', function(response) {
-    let gridReferencedArray = response.getData();
-    //console.log(gridReferencedArray.getGridName());
-    let dataArray = gridReferencedArray.getData();
-    //console.log(gridReferencedArray)
-    let dataValues = dataArray.getDdataList();
-    let csSubset = gridReferencedArray.getCsSubset();
-    //console.log(csSubset);
-    let projection = csSubset.getProjection();
-    //console.log(projection);
-    return dataValues;
+      // setup the handlers for the stream
+      dataStream.on('data', function(response) {
+        let gridReferencedArray = response.getData();
+        //console.log(gridReferencedArray.getGridName());
+        let dataArray = gridReferencedArray.getData();
+        //console.log(gridReferencedArray)
+        let dataValues = dataArray.getDdataList();
+        let csSubset = gridReferencedArray.getCsSubset();
+        //console.log(csSubset);
+        let projection = csSubset.getProjection();
+        //console.log(projection);
+        resolve(dataValues);
+      });
+
+      dataStream.on('status', function(status) {
+        console.log(status);
+      });
+
+      dataStream.on('end', function(end) {
+        console.log('Stream finished!');
+      });
+    } catch (err) {
+      reject(err);
+    }
   });
-
-  dataStream.on('status', function(status) {
-    console.log(status);
-  });
-
-  dataStream.on('end', function(end) {
-    console.log('Stream finished!');
-  });
-  return data;
 }
 /////////////////////////////
 // OpenLayers Mapping Code //
 /////////////////////////////
 
-// Map views always need a projection.  Here we just want to map image
+// Map views always need a projection. Here we just want to map image
 // coordinates directly to map coordinates, so we create a projection that uses
 // the image extent in pixels.
 const width = 1200;
@@ -94,24 +99,28 @@ async function getCanvas(extent, resolution, pixelRatio, imageSize, projection) 
   const ctx =  canvas.getContext("2d");
   try {
     // get the data with grpc magic
-    let data = subset('analysed_sst', 10,5,2,1.5);
-
-    // create new data for the canvas using a Uint8ClampedArray because faster
-    const octets = canvas.width * canvas.height * 4;
-    const pixels = new Uint8ClampedArray(octets);
-    for(let i = 0, di = 0; i < octets; i += 4, di += 1) {
-      // assume we have 1 data value per pixel
-      const rgba = mapDataValueToColor(data); // return [r,g,b,a]
-      pixels[i] = rgba[0];
-      pixels[i + 1] = rgba[1];
-      pixels[i + 2] = rgba[2];
-      pixels[i + 3] = rgba[3];
+    try {
+      let data = await subset('analysed_sst', 10,5,2,1.5);
+      console.log(data)
+      // create new data for the canvas using a Uint8ClampedArray because faster
+      const octets = canvas.width * canvas.height * 4;
+      const pixels = new Uint8ClampedArray(octets);
+      for(let i = 0, di = 0; i < octets; i += 4, di += 1) {
+        // assume we have 1 data value per pixel
+        const rgba = mapDataValueToColor(data); // return [r,g,b,a]
+        pixels[i] = rgba[0];
+        pixels[i + 1] = rgba[1];
+        pixels[i + 2] = rgba[2];
+        pixels[i + 3] = rgba[3];
+      }
+      let imgData = ctx.createImageData(canvas.width, canvas.height);
+      console.log(pixels);
+      // stuff the new data back into the canvas
+      imgData.data.set(pixels);
+      ctx.putImageData(imgData, 0, 0);
+    } catch (err) {
+      console.warn(err);
     }
-    let imgData = ctx.createImageData(canvas.width, canvas.height);
-    console.log(pixels);
-    // stuff the new data back into the canvas
-    imgData.data.set(pixels);
-    ctx.putImageData(imgData, 0, 0);
     return canvas;
   }
   catch (err) {
